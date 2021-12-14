9cc2c6a675b38b5e0eedd3bf9500d6bb6bac8f67
gluknik <gluknik@gmail.com>
2021-12-14 02:32:59 +0200

    Fixes

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ const calculateStrategy = ({initialAmount, period, dayToStartSaving, percentToSa
       amountToHarvest = 0;
 
       return true;
-    } else {
+    } 
+    else {
       if (logs) {
         console.log('|   Can not reinvest. Amount is too small. The strategy may be more effective with bigger initial investment');
       }
@@ -69,7 +70,7 @@ const calculateStrategy = ({initialAmount, period, dayToStartSaving, percentToSa
       amountToHarvest = 0;
       dailyHarvest = 0;
       console.log('----------------------------------------------------------');
-      console.log(`We crashed on day ${i + 1}. Your savings: ${savingsBalance}`);
+      console.log(`We crashed on day ${i + 1}. Your savings: ${savingsBalance}, walletBalance: ${walletBalance}, TOTAL: ${savingsBalance + walletBalance}`);
       break;
     }
 
@@ -94,7 +95,7 @@ const calculateStrategy = ({initialAmount, period, dayToStartSaving, percentToSa
     });
 
     // Check if today we need to decrease our daily withdrow to put some money to savings
-    if (i >= dayToStartSaving) {
+    if (i >= dayToStartSaving && i <= stopReinvestAfter) {
       const currentSavingsPortion = dailyHarvest * (percentToSave / 100);
       savingsBalance += currentSavingsPortion;
       dailyHarvest -= currentSavingsPortion;
@@ -146,18 +147,85 @@ const calculateStrategy = ({initialAmount, period, dayToStartSaving, percentToSa
   }
 };
 
+
+//---- STRATEGY 1
+// With this strategy you invest 300 dollars:
+// - have ability to take 100 dollars that you made in 24 hours on day 43
+// - have ability to take 300 dollars that you made in 24 hours on day 75
+// - start make saving after 60 days that is ok
+// - start collect good savings from day 90 (3 bnb at this spoint)
+//----
+// calculateStrategy({
+//   initialAmount: 0.5, 
+//   period: 180, 
+//   dayToStartSaving: 60, // not relevant if not specified percent to save 
+//   percentToSave: 20, 
+//   crashAfter: 0, // 0 = without crash
+//   logs: true
+// });
+
+//----- STARTEGY 2 - most optimal for the beginning
+// In this case you dont need to worry about crash if it happens after day 90
+//-----
+// calculateStrategy({
+//   initialAmount: 0.5, 
+//   period: 120, // Actually if you chose more than 120 you better continue this strategy till the maximum
+//   dayToStartSaving: 60, // not relevant if not specified percent to save 
+//   percentToSave: 20, 
+//   crashAfter: 100, // 0 = without crash
+//   logs: true
+// });
+
+
+//----- STARTEGY 3 - think bigger
+// Make 1 million dollar in 285 days (in case there is no crash)
+//-----
+// calculateStrategy({
+//   initialAmount: 0.5, 
+//   period: 285, // Actually if you chose more than 120 you better continue this strategy till the maximum
+//   dayToStartSaving: 60, // not relevant if not specified percent to save 
+//   percentToSave: 20, 
+//   crashAfter: 0, // 0 = without crash
+//   logs: true
+// });
+
+
 calculateStrategy({
   initialAmount: 0.5, 
-  period: 60, 
-  dayToStartSaving: 600, // not relevant if not specified percent to save 
-  percentToSave: 0, 
+  period: 285, // Actually if you chose more than 120 you better continue this strategy till the maximum
+  dayToStartSaving: 60, // not relevant if not specified percent to save 
+  percentToSave: 20, 
   crashAfter: 0, // 0 = without crash
   logs: true
 });
 
-// Add option to check if there is a normal bnb amount, check is bnb flow is ok and only
-// after that re-invest money from last harvest
-
+//Ideas:
 // Add option to set an amount of days as a delay between reinvestment
+
 // Add option to set % from the accumulated harvest (after all withdrowals) to reinvest
+
 // Stats, grapghs
+
+// Check if crash happend after the day we started to harvest (and transfer to main wallet daily). If true - 
+// add the harvested amount to amount of savings
+
+//Stop make saving if we on harvest phase -- MAYBE DONE
+
+//Add option make more deposits from outside (for example every month for a fixed amount)
+
+// Make option to part time withdrwals just to make a few extra big savings 
+
+// Inject AI and predict a crash based on social fear in community + statistics :D
+
+
+//Steps:
+//1. Make the code more clear, separate things, avoid complex loops and conditions inside conditions. FIX PROBLEMS WITH MANY NUMBERS. Make shorter. Check why the stratedy ends with positive active balance
+//2. Understand all possible endings and catch them all with tests for each case (including all combinations with savings, crashes etc.)
+//3. Make logical fixes, optimize strategy and find the best possible strategy variations (less risky/more risky/balanced etc. + take as parameter the minimal amount you would like to have if something crashes)
+//4. Connect to web3, try to use this guide -  https://livecodestream.dev/post/interacting-with-smart-contracts-from-web-apps/
+//5. Add option to check if there is a normal bnb amount, check is bnb flow is ok and only after that re-invest money from last harvest
+//6. Maybe add function that shows how many days will take to reach certain amount of money (you provide as parameter) if you invest other amount (one more parameter)
+//7. Check other functions in smart contract to understand if there're other ways to optimize the strategy
+//8. Make all the manipulation with real wallet on BSC (interacting with BNB Harvest contract via web3.js)
+//9. Write a bot that takes your wallet private key and give you to config and make a deposit (also suggest auto plans, maybe on paid basis: less risky - less money / more risky - more money / balanced).
+//10. Make a website with GUI
